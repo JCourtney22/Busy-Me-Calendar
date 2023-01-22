34f9262e88a14e42aa107f2eb12480709c1fa8b2
JCourtney22 <115502589+JCourtney22@users.noreply.github.com>
2023-01-22 11:35:49 +0000

    Added code to JS to save user notes to local storage and for notes to be persisted.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ##Challenges
 
-My first challenge was idenitfying how to place a text box into my HTML. I chose this route to help me recap my HTML and CSS knowledge and to further my understanding of how to work Javascript alongside them. I conducted research and came across this website: https://blog.hubspot.com/website/html-text-box I found that I could use a variety of tags to input a text box. I settled on using the <textarea> tag as I wanted the user to be able to input a range of text as opposed to just a single line. I then worked on implementing the CSS and styling before replicating the container to produce a calendar.
+My first challenge was identifying how to place a text box into my HTML. I chose this route to help me recap my HTML and CSS knowledge and to further my understanding of how to work Javascript alongside them. I conducted research and came across this website: https://blog.hubspot.com/website/html-text-box I found that I could use a variety of tags to input a text box. I settled on using the <textarea> tag as I wanted the user to be able to input a range of text as opposed to just a single line. I then worked on implementing the CSS and styling before replicating the container to produce a calendar.
 
 ##Things I learned
 
--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Current day at the top of page, displayed in the jumbotron
+    //Current day at the top of page, displayed in the jumbotron
 
-var currentDate = moment().format("dddd Do MMMM");
-var displayDate = document.getElementById("currentDay");
-var currentHour = moment().format("HH");
+    var currentDate = moment().format("dddd Do MMMM");
+    var displayDate = document.getElementById("currentDay");
+    var currentHour = moment().format("HH");
 
-displayDate.textContent = currentDate;
+    displayDate.textContent = currentDate;
 
-//
+    //Save user events to local storage
+
+    var eventText = document.getElementById("#hour-9");
+    var saveBtns = document.querySelectorAll("#save");
+
+    //Event Listener
+
+    function saveNote() {
+        console.log($(this))
+        var noteVal = $(this).siblings('.description').val();
+        var id = $(this).siblings('.description').attr('id');
+        
+        localStorage.setItem(id, noteVal);
+    }
+
+    saveBtns.forEach(function (button) {
+        button.addEventListener('click', saveNote);
+    })
+
+    function displayNotes() {
+        for (let index = 9; index <= 17; index++) {
+            var storedNote = localStorage.getItem(`hour-${index}`);
+
+            if (storedNote) {
+                $(`#hour-${index}`).val(storedNote)
+            }
+            
+        }
+    }
+
+    displayNotes();
 
 });
